409625898840823f2b8d6e7cef7d5922d582f565
Francisco <frankiiize.gmail.com>
2021-03-29 03:22:32 -0300

    funcion del tooltip lista para desktop y arregle algunos bugs

--- src/cardScript.js
+++ src/cardScript.js
@@ -1,11 +1,23 @@
 
 const showTooltip =  () =>  {
     let tooltip = document.querySelector('#tooltip');
-    tooltip.className= "cardContainer__share__tooltip";
-    setTimeout(() => tooltip.classList.toggle('change'), 100);
-}
+    if (tooltip.className == "d-none") {
+        tooltip.className = "cardContainer__share__tooltip";
+        setTimeout(() => tooltip.classList.toggle('change'), 100);
+        } else {
+            tooltip.classList.toggle('changeOff');
+            setTimeout( function(){
+                tooltip.className ="d-none";
+            },400);  
+        }
+    }
+
 
 const hideTooltip = () => {
     let tooltip = document.querySelector('#tooltip');
-    setTimeout(() => tooltip.classList.toggle('changeOff'),100);
+    
+    tooltip.classList.toggle('changeOff');
+    setTimeout( function(){
+        tooltip.className ="d-none";
+    },400);   
 }
